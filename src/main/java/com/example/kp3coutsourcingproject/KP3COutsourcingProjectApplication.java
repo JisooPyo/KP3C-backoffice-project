94f5005a33636f4a34bc5b020944c3ff0ad08426
JisooPyo <130378232+JisooPyo@users.noreply.github.com>
2023-07-17 02:06:50 +0900

    project frame (#1)
    프로젝트 틀짜기

---
+++ src/main/java/com/example/kp3coutsourcingproject/KP3COutsourcingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kp3coutsourcingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KP3COutsourcingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KP3COutsourcingProjectApplication.class, args);
+	}
+
+}
