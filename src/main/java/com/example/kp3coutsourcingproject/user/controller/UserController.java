9d2cc3188f3e9ba4afb93c4666f5f74c39591a67
Jongwoo Choi <130136857+Jonggae@users.noreply.github.com>
2023-07-18 09:34:12 +0900

    [Feature] 카카오 소셜 로그인 구현 - 종우 (#23)
    * [init] kakao 로그인 필요한 클래스생성
    
    * [init] User쪽 클래스 생성 (임시)
    
    * [init] RestTemplateConfig 생성
    
    무한 대기 방지
    
    * PR-template-등록 (#20)
    
    * [Update] kakaoService 구현중
    
    * [Update] kakaoService 작성
    
    ---------
    
    Co-authored-by: JisooPyo <130378232+JisooPyo@users.noreply.github.com>

--- src/main/java/com/example/kp3coutsourcingproject/user/controller/UserController.java
+++ src/main/java/com/example/kp3coutsourcingproject/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.kp3coutsourcingproject.user.controller;
 
 import com.example.kp3coutsourcingproject.common.dto.ApiResponseDto;
+import com.example.kp3coutsourcingproject.jwt.JwtUtil;
+import com.example.kp3coutsourcingproject.sociallogin.service.KakaoService;
 import com.example.kp3coutsourcingproject.user.dto.SignupRequestDto;
 import com.example.kp3coutsourcingproject.user.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,26 +23,43 @@ import java.util.List;
 @RequestMapping("/kp3c/user")
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
+    private final KakaoService kakaoService;
+    private final JwtUtil jwtUtil;
 
-	@PostMapping("/signup")
-	@ResponseBody
-	public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult, HttpServletResponse response) {
-		// Validation 예외처리 - signupRequestDto에서 설정한 글자수, 문자규칙(a~z,A~Z,0~9)에 위배되는 경우 fieldError 리스트에 내용이 추가됨
-		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-		StringBuilder errorMessage = new StringBuilder();
+    @PostMapping("/signup")
+    @ResponseBody
+    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult, HttpServletResponse response) {
+        // Validation 예외처리 - signupRequestDto에서 설정한 글자수, 문자규칙(a~z,A~Z,0~9)에 위배되는 경우 fieldError 리스트에 내용이 추가됨
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        StringBuilder errorMessage = new StringBuilder();
 
-		//1건 이상 Validation 관련 에러가 발견된 경우 - 에러메시지(1개~ 여러 개)를 message응답으로 client에 전달
-		if (fieldErrors.size() > 0) {
-			for (FieldError fieldError : bindingResult.getFieldErrors()) {
-				log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-				errorMessage.append(fieldError.getDefaultMessage()).append("\n ");
-			}
-			response.setStatus(400);
-			return ResponseEntity.status(400).body(new ApiResponseDto(errorMessage.toString(), response.getStatus()));
-		}
+        //1건 이상 Validation 관련 에러가 발견된 경우 - 에러메시지(1개~ 여러 개)를 message응답으로 client에 전달
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+                errorMessage.append(fieldError.getDefaultMessage()).append("\n ");
+            }
+            response.setStatus(400);
+            return ResponseEntity.status(400).body(new ApiResponseDto(errorMessage.toString(), response.getStatus()));
+        }
 
-		return ResponseEntity.status(response.getStatus()).body(userService.signup(requestDto, response));
-	}
+        return ResponseEntity.status(response.getStatus()).body(userService.signup(requestDto, response));
+    }
 
+
+    @GetMapping("/user/kakao/callback")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+
+        // code: 카카오 서버로부터 받은 인가코드 service 전달 후 인증처리 및 JWT반환
+        String token = kakaoService.kakaoLogin(code);
+
+        // cookie 생성 및 직접 브라우저에 set
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATOON_HEADER, token.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/";
+    }
 }
+
