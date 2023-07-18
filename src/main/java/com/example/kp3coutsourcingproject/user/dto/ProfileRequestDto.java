c4f5775df4b9417fe65e4a19095061688e0c41a9
jungeun-choi <jungeun5.choi@gmail.com>
2023-07-19 00:45:45 +0900

    [Refactor] ProfileRequestDto: @NotBlank 제거

--- src/main/java/com/example/kp3coutsourcingproject/user/dto/ProfileRequestDto.java
+++ src/main/java/com/example/kp3coutsourcingproject/user/dto/ProfileRequestDto.java
@@ -7,9 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ProfileRequestDto {
-    @NotBlank
     private String nickname;
     private String introduction;
-    @NotBlank
     private String imageUrl;
 }
