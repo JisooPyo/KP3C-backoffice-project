f6240755ae2d774e87e4e8f0f9ef044d09803ec3
Eunbi Kim <eunbkim4@gmail.com>
2023-07-19 03:38:07 +0900

    [Refactor] 팔로우 기능 파일 분리 후 리팩토링

--- src/main/java/com/example/kp3coutsourcingproject/user/controller/UserController.java
+++ src/main/java/com/example/kp3coutsourcingproject/user/controller/UserController.java
@@ -2,9 +2,7 @@ package com.example.kp3coutsourcingproject.user.controller;
 
 import com.example.kp3coutsourcingproject.common.dto.ApiResponseDto;
 import com.example.kp3coutsourcingproject.common.jwt.JwtUtil;
-import com.example.kp3coutsourcingproject.common.security.UserDetailsImpl;
 import com.example.kp3coutsourcingproject.sociallogin.service.KakaoService;
-import com.example.kp3coutsourcingproject.user.dto.ProfileDto;
 import com.example.kp3coutsourcingproject.user.dto.SignupRequestDto;
 import com.example.kp3coutsourcingproject.user.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -13,9 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -28,30 +24,30 @@ import java.util.List;
 @RequestMapping("/kp3c/user")
 public class UserController {
 
-    private final UserService userService;
-    private final KakaoService kakaoService;
+	private final UserService userService;
+	private final KakaoService kakaoService;
 
-    @PostMapping("/signup")
-    @ResponseBody
-    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult, HttpServletResponse response) {
-        // Validation 예외처리 - signupRequestDto에서 설정한 글자수, 문자규칙(a~z,A~Z,0~9)에 위배되는 경우 fieldError 리스트에 내용이 추가됨
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        StringBuilder errorMessage = new StringBuilder();
+	@PostMapping("/signup")
+	@ResponseBody
+	public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult, HttpServletResponse response) {
+		// Validation 예외처리 - signupRequestDto에서 설정한 글자수, 문자규칙(a~z,A~Z,0~9)에 위배되는 경우 fieldError 리스트에 내용이 추가됨
+		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+		StringBuilder errorMessage = new StringBuilder();
 
-        //1건 이상 Validation 관련 에러가 발견된 경우 - 에러메시지(1개~ 여러 개)를 message응답으로 client에 전달
-        if (fieldErrors.size() > 0) {
-            for (FieldError fieldError : bindingResult.getFieldErrors()) {
-                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-                errorMessage.append(fieldError.getDefaultMessage()).append("\n ");
-            }
-            response.setStatus(400);
-            return ResponseEntity.status(400).body(new ApiResponseDto(errorMessage.toString(), response.getStatus()));
-        }
+		//1건 이상 Validation 관련 에러가 발견된 경우 - 에러메시지(1개~ 여러 개)를 message응답으로 client에 전달
+		if (fieldErrors.size() > 0) {
+			for (FieldError fieldError : bindingResult.getFieldErrors()) {
+				log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+				errorMessage.append(fieldError.getDefaultMessage()).append("\n ");
+			}
+			response.setStatus(400);
+			return ResponseEntity.status(400).body(new ApiResponseDto(errorMessage.toString(), response.getStatus()));
+		}
 
-        return ResponseEntity.status(response.getStatus()).body(userService.signup(requestDto, response));
-    }
+		return ResponseEntity.status(response.getStatus()).body(userService.signup(requestDto, response));
+	}
 
-    @GetMapping("/user/kakao/callback")
+    @GetMapping("/kakao/callback")
     public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
 
         // code: 카카오 서버로부터 받은 인가코드 service 전달 후 인증처리 및 JWT반환
@@ -63,29 +59,5 @@ public class UserController {
         response.addCookie(cookie);
 
         return "redirect:/";
-    }
-
-    @GetMapping("/{username}/followers")
-    public ResponseEntity<List<ProfileDto>> getFollowers(@PathVariable String username) {
-        List<ProfileDto> followers = userService.getFollowers(username);
-        return ResponseEntity.ok().body(followers);
-    }
-
-    @GetMapping("/{username}/followees")
-    public ResponseEntity<List<ProfileDto>> getFollowees(@PathVariable String username) {
-        List<ProfileDto> followees = userService.getFollowees(username);
-        return ResponseEntity.ok().body(followees);
-    }
-
-    @PostMapping("/follow/{username}")
-    public ResponseEntity<ApiResponseDto> follow(@PathVariable String username, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        userService.follow(username, userDetails.getUser());
-        return ResponseEntity.ok().body(new ApiResponseDto("팔로우 성공", HttpStatus.OK.value()));
-    }
-
-    @PostMapping("/unfollow/{username}")
-    public ResponseEntity<ApiResponseDto> unfollow(@PathVariable String username, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        userService.unfollow(username, userDetails.getUser());
-        return ResponseEntity.ok().body(new ApiResponseDto("언팔로우 성공", HttpStatus.OK.value()));
     }
 }
