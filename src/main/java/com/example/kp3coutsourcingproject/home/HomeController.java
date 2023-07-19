92c5c8df8efdc1dd912b5261eb5f470a9d0a77a9
Jongwoo Choi <muvnelik@naver.com>
2023-07-19 11:04:22 +0900

    [Update] test용 HomeController

--- src/main/java/com/example/kp3coutsourcingproject/home/HomeController.java
+++ src/main/java/com/example/kp3coutsourcingproject/home/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/kp3c/user/login-page")
     public String home() {
         return "index";
     }
