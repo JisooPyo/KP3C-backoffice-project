3ab920f0632171b7d2e8467295ae4ad9ca48845e
Eunbi Kim <eunbkim4@gmail.com>
2023-07-19 10:03:21 +0900

    [Fix] UserController 코드 정렬

--- src/main/java/com/example/kp3coutsourcingproject/user/controller/UserController.java
+++ src/main/java/com/example/kp3coutsourcingproject/user/controller/UserController.java
@@ -24,28 +24,28 @@ import java.util.List;
 @RequestMapping("/kp3c/user")
 public class UserController {
 
-	private final UserService userService;
-	private final KakaoService kakaoService;
+    private final UserService userService;
+    private final KakaoService kakaoService;
 
-	@PostMapping("/signup")
-	@ResponseBody
-	public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult, HttpServletResponse response) {
-		// Validation 예외처리 - signupRequestDto에서 설정한 글자수, 문자규칙(a~z,A~Z,0~9)에 위배되는 경우 fieldError 리스트에 내용이 추가됨
-		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-		StringBuilder errorMessage = new StringBuilder();
+    @PostMapping("/signup")
+    @ResponseBody
+    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult, HttpServletResponse response) {
+        // Validation 예외처리 - signupRequestDto에서 설정한 글자수, 문자규칙(a~z,A~Z,0~9)에 위배되는 경우 fieldError 리스트에 내용이 추가됨
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        StringBuilder errorMessage = new StringBuilder();
 
-		//1건 이상 Validation 관련 에러가 발견된 경우 - 에러메시지(1개~ 여러 개)를 message응답으로 client에 전달
-		if (fieldErrors.size() > 0) {
-			for (FieldError fieldError : bindingResult.getFieldErrors()) {
-				log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-				errorMessage.append(fieldError.getDefaultMessage()).append("\n ");
-			}
-			response.setStatus(400);
-			return ResponseEntity.status(400).body(new ApiResponseDto(errorMessage.toString(), response.getStatus()));
-		}
+        //1건 이상 Validation 관련 에러가 발견된 경우 - 에러메시지(1개~ 여러 개)를 message응답으로 client에 전달
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+                errorMessage.append(fieldError.getDefaultMessage()).append("\n ");
+            }
+            response.setStatus(400);
+            return ResponseEntity.status(400).body(new ApiResponseDto(errorMessage.toString(), response.getStatus()));
+        }
 
-		return ResponseEntity.status(response.getStatus()).body(userService.signup(requestDto, response));
-	}
+        return ResponseEntity.status(response.getStatus()).body(userService.signup(requestDto, response));
+    }
 
     @GetMapping("/kakao/callback")
     public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
