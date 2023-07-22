572a30af640c6f8fef3a236044dc85cc3ad8790f
hyewon218 <won_11111@naver.com>
2023-07-22 13:22:50 +0900

    @ModelAttribute 로 값 받아올 때 PostRequestDto 에 @Setter 어노테이션 필요

--- src/main/java/com/example/kp3coutsourcingproject/post/dto/PostRequestDto.java
+++ src/main/java/com/example/kp3coutsourcingproject/post/dto/PostRequestDto.java
@@ -2,7 +2,9 @@ package com.example.kp3coutsourcingproject.post.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
 public class PostRequestDto {
 	@NotNull(message = "내용을 입력하세요.")
