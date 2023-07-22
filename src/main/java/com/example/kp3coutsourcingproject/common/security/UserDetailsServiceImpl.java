c9e0994c97bce9a4af94bb9b8a8edaa35e3f53db
jungeun-choi <jungeun5.choi@gmail.com>
2023-07-23 00:39:48 +0900

    [Complete] Username과 Email 두 가지로 로그인하기

--- src/main/java/com/example/kp3coutsourcingproject/common/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/kp3coutsourcingproject/common/security/UserDetailsServiceImpl.java
@@ -15,9 +15,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	private final UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findByEmail(email)
-				.orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+		User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+				.orElseThrow(() -> new UsernameNotFoundException("Not Found " + usernameOrEmail));
 
 		return new UserDetailsImpl(user);
 	}
--- src/main/java/com/example/kp3coutsourcingproject/user/repository/UserRepository.java
+++ src/main/java/com/example/kp3coutsourcingproject/user/repository/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
     Optional<User> findByKakaoId(Long kakaoId);
 }
